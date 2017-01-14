a228a2518a1afefba9c2489723832f0e49a810e1
Siong Ong <siong1987@gmail.com>
2017-01-14 14:37:06 -0800

    Initial Commit

---
+++ Volume/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Volume
+//
+//  Created by Teng Siong Ong on 1/14/17.
+//  Copyright © 2017 Teng Siong Ong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
